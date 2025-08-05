20c4439c2a64077202a2e5bd219e819e1d021f5f
Maihetemy <maihetemy@gmail.com>
2025-08-05 21:51:17 +0300

    update structure and login api & context

---
+++ src/pages/Categories.jsx
@@ -0,0 +1,32 @@
+import {useEffect} from "react";
+import useCategories from "../Hooks/useCategories";
+// import { cartContext } from "./../../context/CartContext";
+import { Link } from "react-router-dom";
+export default function Categories() {
+  let { data } = useCategories();
+  // let { addToCart } = useContext(cartContext);
+  useEffect(() => {}, []);
+  return (
+    <>
+      <h1 className="my-10 font-bold text-3xl text-green-950">Categories</h1>
+      <div className="row">
+        {data?.map((category) => (
+          <div key={category._id} className="w-1/2 md:w-1/3 lg:w-1/4">
+            <Link to={`/category/${category.name}`}>
+              <div className="p-2 flex justify-center items-center flex-wrap cursor-pointer">
+                <h2 className="my-3 text-xl font-semibold text-green-900">
+                  {category.name}
+                </h2>
+                <img
+                  className="h-[250px] w-[250px]"
+                  src={category.image}
+                  alt={category.name}
+                />
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
